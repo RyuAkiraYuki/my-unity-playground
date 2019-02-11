abae4b1b4c297f2dcff1057c71763a2b7958e219
RyuAkiraYuki <rurounikenshinyuki@gmail.com>
2019-02-11 02:01:09 +0200

    ZigZag game

---
+++ ZigZag/Assets/Scripts/BallController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    [SerializeField]
+    private float speed;
+    Rigidbody rigBdy;
+
+    bool isStarted;
+    bool isGameOver;
+
+
+    void Awake() {
+        rigBdy = GetComponent<Rigidbody>();
+    }
+
+    // Use this for initialization
+    void Start() {
+        isStarted = false;
+        isGameOver = false;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (!isStarted) {
+            if (Input.GetMouseButtonDown(0)) {
+                rigBdy.velocity = new Vector3(speed, 0, 0);
+                isStarted = true;
+            }
+        }
+
+        Debug.DrawRay(transform.position, Vector3.down, Color.red);
+
+        if (!Physics.Raycast(transform.position, Vector3.down, 1f)) {
+            isGameOver = true;
+            rigBdy.velocity = new Vector3(0, -speed * 3, 0);
+
+            Camera.main.GetComponent<CameraFollow>().isGameOver = true;
+        }
+
+        if (Input.GetMouseButtonDown(0) && !isGameOver) {
+            SwitchDirection();
+        }
+    }
+
+    void SwitchDirection() {
+        if (rigBdy.velocity.z > 0) {
+            rigBdy.velocity = new Vector3(speed, 0, 0);
+        } else if (rigBdy.velocity.x > 0) {
+            rigBdy.velocity = new Vector3(0, 0, speed);
+        }
+    }
+}
---
+++ ZigZag/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public GameObject ball;
+    Vector3 vOffset;
+
+    public float lerpRate;
+
+    public bool isGameOver;
+
+    // Use this for initialization
+    void Start() {
+        vOffset = ball.transform.position - transform.position;
+        isGameOver = false;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (!isGameOver) {
+            Follow();
+        }
+    }
+
+    void Follow() {
+        Vector3 curPos = transform.position;
+        Vector3 targetPos = ball.transform.position - vOffset;
+        curPos = Vector3.Lerp(curPos, targetPos, lerpRate * Time.deltaTime);
+        transform.position = curPos;
+    }
+}
