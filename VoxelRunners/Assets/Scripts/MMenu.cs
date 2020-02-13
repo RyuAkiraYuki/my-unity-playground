3d947fac30e7d6b9334a348ed30a1e6234736ac9
Igor Alhasov <rurounikenshinyuki@gmail.com>
2020-02-13 18:09:15 +0200

    MainMenu done
    Done with main menu
    Without animated character

---
+++ VoxelRunners/Assets/Scripts/MMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MMenu : MonoBehaviour {
+
+    public string targetScene;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void PlayGeme() {
+        SceneManager.LoadScene(targetScene);
+    }
+}
